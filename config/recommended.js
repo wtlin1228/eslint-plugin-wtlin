4773af3b7217676be82b3a6006af0f2a466d137b
wtlin1228 <wtlin1228@gmail.com>
2021-08-12 01:09:20 +0800

    feat: add recommended canned config

---
+++ config/recommended.js
@@ -0,0 +1,18 @@
+/**
+ * The basics.
+ * @type {Object}
+ */
+module.exports = {
+  plugins: ["wtlin"],
+
+  rules: {
+    "wtlin/limit-continuous-import-declarations": 2,
+  },
+
+  // need all these for parsing dependencies (even if _your_ code doesn't need
+  // all of them)
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export const rules = {
   "limit-continuous-import-declarations": require("./lib/rules/limit-continuous-import-declarations"),
 };
+
+export const configs = {
+  recommended: require("../config/recommended"),
+};
